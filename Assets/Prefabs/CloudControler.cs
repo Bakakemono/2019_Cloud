ed6bb6d4e35d1795fc1d34ed422d55fc10eb9126
Bakakemono <robinalves1224@gmail.com>
2019-02-14 22:15:58 +0100

    Base for project

---
+++ Assets/Prefabs/CloudControler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudControler : MonoBehaviour
+{
+#region
+
+#endregion
+
+    private float playerForce = 1.0f;
+
+    void Start()
+    {
+
+    }
+}
