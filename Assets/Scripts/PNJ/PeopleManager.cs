90176d8d476d0b2b22aa639ed3dc1423754950c9
Bakakemono <robinalves1224@gmail.com>
2019-02-15 15:57:53 +0100

    Begin add of praise the sun and other thing

--- Assets/Scripts/PNJ/PeopleManager.cs
+++ Assets/Scripts/PNJ/PeopleManager.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PeopleManager : MonoBehaviour
 {
+    #region Gizmos
     [SerializeField] private Vector2 left = new Vector2(-10, -4);
     [SerializeField] private Vector2 right = new Vector2(10, -4);
+    #endregion
+
+    [SerializeField] private GameObject Peon;
+    [SerializeField] private GameObject PraiseTheSun;
+
+
+
 
     void OnDrawGizmos()
     {
         Gizmos.color = Color.black;
 
-        Gizmos.DrawSphere(left, 0.2f);
-        Gizmos.DrawSphere(right, 0.2f);
+        Gizmos.DrawWireSphere(left, 0.2f);
+        Gizmos.DrawWireSphere(right, 0.2f);
 
         Gizmos.color = Color.red;
 
